22789eb9f9227df1bb55399f5a0cc137d19de905
Gogmazios <732861@student.dusd.net>
2026-05-13 15:19:59 -0700

    chslleng 5
    C5 add timer/count down

---
+++ P7Unit5Saucedo/Assets/Challenge 5/Scripts/Timeee.cs
@@ -0,0 +1,61 @@
+//using UnityEngine;
+//us/ing System.Collections.Generic;
+//using System.Collections;
+//using TMPro;
+//using UnityEngine.SceneManagement;
+//using UnityEngine.UI;
+
+//public class Timeee : MonoBehaviour
+////{
+//    public float StartTime;
+//    private float TimeLeft;
+//
+//    public TMP_Text Timertext;
+//    public TMP_Text Timertextt;
+ //   public TMP_Text Timertexttt;
+ //
+ //   public Button Easy;
+ //   public Button Medium;
+ //   public Button Hard; 
+ //   // Start is called once before the first execution of Update after the MonoBehaviour is created
+ //   void Start()
+ //   {
+        
+ //   }
+
+ //   // Update is called once per frame
+ //   void Update()
+ //   {
+  //      if(TimeLeft > 0)
+  //      {
+  //          TimeLeft -= Time.deltaTime;
+  //          Timertext = TimerLeft.ToString("0.00");
+  //          Timertextt = TimerLeft.ToString("0.00");
+  //          Timertexttt = TimerLeft.ToString("0.00");
+  //      }
+  //      else
+   //     {
+  //          Easy.gameObject.SetActive(true);
+  //          Medium.gameObject.SetActive(true); 
+   //         Hard.gameObject.SetActive(true);
+
+  //          Timertext.gameObject.SetActive(true);
+  //          Timertextt.gameObject.SetActive(true);
+  //          Timertexttt.gameObject.SetActive(true);
+
+  //      }
+  //  }
+
+  //  public void StartClicked()
+  //  {
+  //      TimeLeft = StartTime; 
+  //      Easy.gameObject.SetActive(false);
+   //     Medium.gameObject.SetActive(false);
+   //     Hard.gameObject.SetActive(false);
+
+  //      Timetext.gameObject.SetActive(true);
+  //      Timetextt.gameObject.SetActive(true);
+  //      Timetexttt.gameObject.SetActive(true);
+  //  }
+
+//}
